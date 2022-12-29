95981ef4fb3634e768d3457b5121f4a35d52a87e
fathurhidayat3 <fathur.hidayat@gmail.com>
2022-12-30 05:46:02 +0700

    feat: add link navbar title

--- src/app/Common/components/Navbar/index.tsx
+++ src/app/Common/components/Navbar/index.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 
 import styles from "./navbar.module.css";
 import SearchBox from "../SearchBox";
+import { Link } from "react-router-dom";
+import { RoutePaths } from "../../../router";
 
 export default function Navbar(props: any): React.ReactElement {
   const { keyword, setKeyword, onClick } = props;
@@ -9,7 +11,9 @@ export default function Navbar(props: any): React.ReactElement {
   return (
     <div className={styles["navbar"]}>
       <div>
-        <h3>Anime Library</h3>
+        <h3>
+          <Link to={RoutePaths.HOME}>Anime Library</Link>
+        </h3>
         <SearchBox
           keyword={keyword}
           setKeyword={setKeyword}
